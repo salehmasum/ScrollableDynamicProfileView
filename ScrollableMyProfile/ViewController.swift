66ace1ac958f35bf84af1c9f3b6a8749820fe773
Saleh Masum <salehmasum@gmail.com>
2022-09-09 16:49:31 +1000

    Initial Commit

---
+++ ScrollableMyProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollableMyProfile
+//
+//  Created by Saleh Masum on 9/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
